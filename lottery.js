52a1cf730958710ae2263132231fa24c0d6a01d1
AL_WUHC <wuhc2@asiainfo-linkage.com>
2015-01-21 13:46:42 +0800

    jsonp

--- lottery.js
+++ lottery.js
@@ -1,26 +1,27 @@
-data=[
-         {
-             "name" : "三等奖",
-             "memb" : [
-                 "AAA3",
-                 "BBB3",
-                 "CCC3"
-             ]
-         },
-         {
-             "name" : "二等奖",
-             "memb" : [
-                 "AAA2",
-                 "BBB2",
-                 "CCC2"
-             ]
-         },
-         {
-             "name" : "一等奖",
-             "memb" : [
-                 "AAA1",
-                 "BBB1",
-                 "CCC1"
-             ]
-         }
-     ];
+{"data" : [
+              {
+                  "name" : "三等奖",
+                  "memb" : [
+                      "AAA3",
+                      "BBB3",
+                      "CCC3"
+                  ]
+              },
+              {
+                  "name" : "二等奖",
+                  "memb" : [
+                      "AAA2",
+                      "BBB2",
+                      "CCC2"
+                  ]
+              },
+              {
+                  "name" : "一等奖",
+                  "memb" : [
+                      "AAA1",
+                      "BBB1",
+                      "CCC1"
+                  ]
+              }
+          ];
+}
