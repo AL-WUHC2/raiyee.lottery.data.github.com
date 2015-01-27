1d4a43593a350aa429c8a6d1f183d9e6206ebf37
AL_WUHC <wuhc2@asiainfo-linkage.com>
2015-01-27 11:21:47 +0800

    test data.

--- lottery.js
+++ lottery.js
@@ -1,18 +1,6 @@
-lottery_data({"lotteryData" : [{"name" : "三等奖",
-                                "memb" : [
-                                    "AAA3", "BBB3", "CCC3", "DDD3", "EEE3", "FFF3", "GGG3",
-                                    "HHH3", "III3", "JJJ3", "KKK3", "LLL3", "MMM3", "NNN3"
-                                ]},
-                               {"name" : "二等奖",
-                                "memb" : [
-                                    "AAA2", "BBB2", "CCC2", "DDD2", "EEE2", "FFF2", "GGG2",
-                                    "HHH2", "III2", "JJJ2", "KKK2", "LLL2", "MMM2", "NNN2"
-                                ]},
-                               {"name" : "一等奖",
-                                "memb" : [
-                                    "AAA1", "BBB1", "CCC1", "DDD1", "EEE1", "FFF1", "GGG1",
-                                    "HHH1", "III1", "JJJ1", "KKK1", "LLL1", "MMM1", "NNN1"
-                                ]}
+lottery_data({"lotteryData" : [{"name" : "三等奖"},
+                               {"name" : "二等奖"},
+                               {"name" : "一等奖"}
                              ],
-             "members" : ["ERROR1", "ERROR2", "ERROR3", "ERROR4", "ERROR5"]
+             "members" : ["李晓莉", "童剑", "冯雨", "杜颖", "牛洋", "刘雷", "高禹", "付强", "严宾", "袁帅帅", "李栋栋", "耿晋普", "刘华洋", "冯高飞", "闫永红", "周宇", "孙俊", "曹盛", "刘嗣颉", "张发官", "熊震霏", "王彬彬", "陈江南", "吉思静", "张鑫", "陈龙", "刘明玥", "汪沁", "宋瑶", "朱元强", "储韩菲", "桂冠"]
             });
