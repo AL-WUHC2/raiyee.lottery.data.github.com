cfe47abf04c12d536afda6bcbee0223622d8bb04
AL_WUHC <wuhc2@asiainfo-linkage.com>
2015-01-27 11:04:08 +0800

    commit.

--- lottery.js
+++ lottery.js
@@ -1,23 +1,17 @@
-lottery_data({"lotteryData" : [ {"name" : "三等奖",
+lottery_data({"lotteryData" : [{"name" : "三等奖",
                                 "memb" : [
                                     "AAA3", "BBB3", "CCC3", "DDD3", "EEE3", "FFF3", "GGG3",
-                                    "HHH3", "III3", "JJJ3", "KKK3", "LLL3", "MMM3", "NNN3",
-                                    "OOO3", "PPP3", "QQQ3", "RRR3", "SSS3", "TTT3",
-                                    "UUU3", "VVV3", "WWW3", "XXX3", "YYY3", "ZZZ3"
+                                    "HHH3", "III3", "JJJ3", "KKK3", "LLL3", "MMM3", "NNN3"
                                 ]},
                                {"name" : "二等奖",
                                 "memb" : [
                                     "AAA2", "BBB2", "CCC2", "DDD2", "EEE2", "FFF2", "GGG2",
-                                    "HHH2", "III2", "JJJ2", "KKK2", "LLL2", "MMM2", "NNN2",
-                                    "OOO2", "PPP2", "QQQ2", "RRR2", "SSS2", "TTT2",
-                                    "UUU2", "VVV2", "WWW2", "XXX2", "YYY2", "ZZZ2"
+                                    "HHH2", "III2", "JJJ2", "KKK2", "LLL2", "MMM2", "NNN2"
                                 ]},
                                {"name" : "一等奖",
                                 "memb" : [
                                     "AAA1", "BBB1", "CCC1", "DDD1", "EEE1", "FFF1", "GGG1",
-                                    "HHH1", "III1", "JJJ1", "KKK1", "LLL1", "MMM1", "NNN1",
-                                    "OOO1", "PPP1", "QQQ1", "RRR1", "SSS1", "TTT1",
-                                    "UUU1", "VVV1", "WWW1", "XXX1", "YYY1", "ZZZ1"
+                                    "HHH1", "III1", "JJJ1", "KKK1", "LLL1", "MMM1", "NNN1"
                                 ]}
                              ],
              "members" : ["ERROR1", "ERROR2", "ERROR3", "ERROR4", "ERROR5"]
